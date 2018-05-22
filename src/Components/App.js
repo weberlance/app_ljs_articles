0ad725b9736d5ef97a7dceee571ec186751c4b64
andrew.petin <andrew.petin@effective-soft.com>
2018-05-22 10:50:03 +0300

    add simple router

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,6 +8,8 @@ import UserForm from './UserForm';
 import Counter from './Counter';
 import Filters from './Filters'
 
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+
 export default class App extends React.Component {
   static propTypes = {
 
@@ -16,12 +18,21 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store = {store}>
-        <div>
-          <UserForm />
-          <Counter />
-          <Filters />
-          <ArticleList/>
-        </div>
+        <Router>
+          <div>
+            <div>
+              <h1>MainMenu</h1>
+              <div><NavLink activeStyle={{color:"#f55"}} to="/counter">Counter</NavLink></div>
+              <div><NavLink activeStyle={{color:"#f55"}} to="/filters">Filters</NavLink></div>
+              <div><NavLink activeStyle={{color:"#f55"}} to="/articles">Articles</NavLink></div>
+            </div>
+
+            <UserForm />
+            <Route path = "/counter" component = {Counter}/>
+            <Route path = "/filters" component = {Filters}/>
+            <Route path = "/articles" component = {ArticleList}/>
+          </div>
+        </Router>
       </Provider>
     );
   }
